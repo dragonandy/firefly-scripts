55916f7ea8f3de1eaf127c90bcd53370d42fc907
firefly <dragonandy@foxmail.com>
2018-11-02 13:15:54 +0800

    add echo hint in the scripts

--- fix_ldconfig/fix_ldconfig_manually.sh
+++ fix_ldconfig/fix_ldconfig_manually.sh
@@ -3,9 +3,13 @@ DIR=/usr/lib/arm-linux-gnueabihf
 PATTEN=libgst*so.0
 CHANGE=.1004.0
 
+echo "run with sudo or you may not have permission"
+
 cd $DIR
 for FILE in $PATTEN
     do
     echo $FILE
+    echo " link to "
+    echo $FILE$CHANGE
     ln -sf $FILE$CHANGE $FILE
     done
