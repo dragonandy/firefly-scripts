4b0791087b6383312315a1c7b3acc16005827e1e
firefly <dragonandy@foxmail.com>
2018-11-04 12:47:55 +0800

    if fstab already added the swap mount setting

--- make_swap/make_swap.sh
+++ make_swap/make_swap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=/mnt/
 SWAPFILE=swapfile
-SWAPSIZE=2048
+SWAPSIZE=4096
 SIZEUNIT=1M
 
 echo -en "\\033[1;34m"
@@ -16,13 +16,21 @@ swapoff -a
 chmod 0600 $DIR$SWAPFILE
 swapon $DIR$SWAPFILE
 
-echo -en "\\033[1;33m"
-echo "Warning swap mount setting will be written to /etc/fstab"
-echo "If you prefer manually mount the swapfile,delete the line:"
-echo $DIR$SWAPFILE "swap swap default 0 0"
-echo "from the fstab file"
-echo -en "\\033[0;39m"
-
-echo $DIR$SWAPFILE "swap swap defaults 0 0">>/etc/fstab
+if cat /etc/fstab | grep -q $DIR$SWAPFILE;then
+    echo -en "\\033[1;33m"
+    echo "swap mount setting will not be written to /etc/fstab"
+    echo "you can manually add"
+    echo $DIR$SWAPFILE "swap swap default 0 0"
+    echo "to /etc/fstab"
+    echo -en "\\033[0;39m"
+else
+    echo -en "\\033[1;33m"
+    echo "Warning swap mount setting will be written to /etc/fstab"
+    echo "If you prefer manually mount the swapfile,delete the line:"
+    echo $DIR$SWAPFILE "swap swap default 0 0"
+    echo "from the fstab file"
+    echo -en "\\033[0;39m"
+    echo $DIR$SWAPFILE "swap swap defaults 0 0">>/etc/fstab
+fi
 
 sysctl vm.swappiness=60
